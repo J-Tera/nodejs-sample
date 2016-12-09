686842783fe09d9d6b5f1bef08c707f2a0d4f414
Junya Terada <tjun@jp.ibm.com>
2016-12-09 12:26:38 +0900

    Updated to use session

--- sample/server.js
+++ sample/server.js
@@ -1,5 +1,5 @@
 /**
- * Server for the GuessTheWord app
+ * Server for the Sample app
  */
 
 var express = require('express');
@@ -10,7 +10,7 @@ var http = require('http');
 var host = "localhost";
 var port = 3030;
 var cloudant = {
-		 		 url : "https://f5452bb8-7d86-4198-9fa1-76a9d0a55727-bluemix:6130b19398e8fd33feaab49df054638945906fc47fe22bb25b8035a4eee31d6e@f5452bb8-7d86-4198-9fa1-76a9d0a55727-bluemix.cloudant.com" // TODO: Update		 		 
+  url : "https://f5452bb8-7d86-4198-9fa1-76a9d0a55727-bluemix:6130b19398e8fd33feaab49df054638945906fc47fe22bb25b8035a4eee31d6e@f5452bb8-7d86-4198-9fa1-76a9d0a55727-bluemix.cloudant.com" // TODO: Update		 		 
 };
 
 
@@ -29,51 +29,31 @@ if (process.env.hasOwnProperty("VCAP_SERVICES")) {
 var nano = require('nano')(cloudant.url);
 var db = nano.db.use('guess_the_word_hiscores');
 
-
-
-
-/**
- * Lookup the word in the wordnik online dictionary and return a description for it.
- * @param word {String} Word to lookup description for
- * @param cb_description {function} Callback with the description as argument. 
- * If the word was not found in the dictionary the description is empty.
+//Session
+var cookieParser = require('cookie-parser');
+app.use(cookieParser()); 
+var bodyParser = require('body-parser');
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
  */
-function wordLookup(word, cb_description) {
-  http.request(
-      {
-	host: "api.wordnik.com",
-	path: "/v4/word.json/" + word + "/definitions?limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
-      },	function (res) {
-	var str = '';
-	res.on('data', function(d) {
-	  str += d;
-	});
-	res.on('end', function() {
-	  var wordList = JSON.parse(str);
-	  cb_description(wordList.length > 0 ? wordList[0].text : "");
-	});
-      }).end();
-}
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get('/randomword', function(request, response) {
-  http.request(
-      {
-	host: "api.wordnik.com",
-	path: "/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
-      }, function (res) {
-	var str = '';
-	res.on('data', function(d) {
-	  str += d;
-	});
-	res.on('end', function() {
-	  var wordObj = JSON.parse(str);
-	  wordLookup(wordObj.word, function(descr) {
-	    var randomWordObj = { word : wordObj.word, description : descr };
-	    response.send(JSON.stringify(randomWordObj));		
-	  });								
-	});
-      }).end();
-});
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+var session = require('express-session');
+app.use(session({
+    secret: 'secret',
+    cookie: {
+        httpOnly: false,
+        maxAge: new Date(Date.now() + 60 * 60 * 1000)
+    }
+})); //追加
 
 // Set path to Jade template directory
 app.set('views', __dirname + '/views');
@@ -96,40 +76,23 @@ app.use(express.static(__dirname + '/public'));
 app.engine('ect', ECT({ watch: true, root: __dirname + '/views', ext: '.ect' }).render);
 app.set('view engine', 'ect');
 
-// Bind the root '/' URL to the hiscore page
+// Bind the root '/' URL
 app.get('/', function(req, res){
-  res.render('hiscores.ect', {title: 'Hiscores'});
+  var name = "";
+  if( req.session.name && req.session.name != "") {
+	  name = req.session.name;
+  }
+  res.render('index.ect', {title: 'Login ' + name, "name" : name});
 });
 
-// Bind the '/play' URL to the main game page
-app.get('/play', function(req, res){	
-  res.render('main.ect', {title: 'Guess the Word'});
+// Bind the '/login' URL
+app.post('/login', function(req, res){
+  var name = req.body.name;
+  req.session.name = name;
+  res.render('main.ect', {title: 'Hello ' +name });
 });
 
-app.get('/hiscores', function(request, response) {
-  db.view('top_scores', 'top_scores_index', function(err, body) {
-  if (!err) {
-    var scores = [];
-      body.rows.forEach(function(doc) {
-        scores.push(doc.value);		      
-      });
-      response.send(JSON.stringify(scores));
-	}
-  });
-});
-
-app.get('/save_score', function(request, response) {
-  var name = request.query.name;
-  var score = request.query.score;
-
-  var scoreRecord = { 'name': name, 'score' : parseInt(score), 'date': new Date() };
-  db.insert(scoreRecord, function(err, body, header) {
-    if (!err) {       
-      response.send('Successfully added one score to the DB');
-    }
-  });
-});
-
+// Server start
 var server = app.listen(port, function() {
   console.log('Server running on port %d on host %s', server.address().port, host);
 });
